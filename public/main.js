df1a54f8f355bdeb64578aca252107662c443172
Richard Tran <RTran904@gmail.com>
2023-11-05 23:53:02 -0800

    Finish intro css, now on to middle

--- public/main.js
+++ public/main.js
@@ -1,16 +1,70 @@
-console.log("Add your visualizations here!");
-
-window.addEventListener('scroll', () => {
-    let mountains = document.getElementsByClassName("parallax-layer");
+// Parallax effect for foreground trees.
+window.addEventListener("scroll", () => {
     let top = document.documentElement.scrollTop;
-    // for (const elt of mountains) {
-    //     // elt.style.opacity = 1 - top / 1920;
-    // }
+    let layerShadow = document.querySelector(".layer-shadow");
+    let trees = document.querySelector(".layer-trees");
+    layerShadow.style.opacity = top / 900;
+    trees.style.transform = "translateY(-" + top  + "px)";
+});
 
-    let chicken = document.querySelector('.chicken');
-    chicken.style.opacity = top / 1000;
+// Callback function on header click. Initiates fade in on intro elements.
+function introClickCallback() {
+    let header = document.querySelector("header");
 
-    let front = document.querySelector('.layer-front');
-    front.style.transform = 'translateY(-' + top  + "px)";
-    console.log(front.style.transform);
-})
+    header.style.animationName = "fadeOut";
+    header.style.animationDuration = "2s";
+    header.style.animationFillMode = "forwards";
+
+    setTimeout(() => {
+        let main = document.querySelector("main");
+        main.style.display = "block";
+        main.style.animationName = "fadeIn";
+        main.style.animationDuration = "3s";
+        main.style.animationFillMode = "forwards";
+    }, 2500);
+
+    setTimeout(() => {
+        let fact = document.querySelector(".fact-1");
+        fact.style.display = "block";
+        fact.style.animationName = "fadeIn";
+        fact.style.animationDuration = "2s";
+        fact.style.animationFillMode = "forwards";
+    }, 4000)
+
+    setTimeout(() => {
+        let fact = document.querySelector(".fact-2");
+        fact.style.display = "block";
+        fact.style.animationName = "fadeIn";
+        fact.style.animationDuration = "2s";
+        fact.style.animationFillMode = "forwards";
+    }, 6000)
+
+    setTimeout(() => {
+        let scrollGuide = document.querySelector(".scroll-guide");
+        scrollGuide.style.animationName = "fadeIn";
+        scrollGuide.style.animationDuration = "2s";
+        scrollGuide.style.animationFillMode = "forwards";
+    }, 9000)
+};
+
+let header = document.querySelector("header");
+let mediaQuery = window.matchMedia("(min-width: 992px)");
+console.log(mediaQuery);
+let introPrompt = document.createElement("p");
+introPrompt.classList.add("pad-title");
+// Changes the text on intro screen depending on media query.
+if (mediaQuery.matches) {
+    introPrompt.textContent = "Click to Explore";
+} else {
+    introPrompt.textContent = "Touch to Explore";
+}
+header.querySelector(".header-content").appendChild(introPrompt);
+mediaQuery.addEventListener("change", (e) => {
+    if (e.matches) {
+        introPrompt.textContent = "Click to Explore";
+    } else {
+        introPrompt.textContent = "Touch to Explore";
+    }   
+});
+header.addEventListener("click", introClickCallback, {once: true});
+
