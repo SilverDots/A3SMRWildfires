33be4f44b9ebe55b06b66506ba5b7f1f81658896
Richard Tran <RTran904@gmail.com>
2023-11-05 17:51:16 -0800

    Working on web page css

--- public/main.js
+++ public/main.js
@@ -1,1 +1,16 @@
 console.log("Add your visualizations here!");
+
+window.addEventListener('scroll', () => {
+    let mountains = document.getElementsByClassName("parallax-layer");
+    let top = document.documentElement.scrollTop;
+    // for (const elt of mountains) {
+    //     // elt.style.opacity = 1 - top / 1920;
+    // }
+
+    let chicken = document.querySelector('.chicken');
+    chicken.style.opacity = top / 1000;
+
+    let front = document.querySelector('.layer-front');
+    front.style.transform = 'translateY(-' + top  + "px)";
+    console.log(front.style.transform);
+})
